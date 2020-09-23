ea866aa29c4fd181394f94d9694d2fba249f9edf
Juan Sebastian Gonzalez Rojas <js.gonzalez15@uniandes.edu.co>
2020-09-23 06:18:56 -0500

    funciona ocultar tabla y desplegarla SOLO una vez, funciona agregar y contar los productos escogidos... YA CASI HP

--- restaurantLogic.js
+++ restaurantLogic.js
@@ -12,7 +12,6 @@ let pA = new Promise((resolve, reject) => {
     };
     reqA.send()
 })
-let nombreBotonesCategorias=[];
 
 /** Ejecucion inicial de Promesa Javascript 
  * Agrega la categoria actual como titulo 
@@ -25,9 +24,7 @@ pA.then((respuestaA) => {
     displayCards(jsonResponseA[0]["products"]); //actualización de listado actual
 
     for (i = 0; i < jsonResponseA.length; i++) {
-        console.log(jsonResponseA[i]);
-        addCategoria(i,jsonResponseA[i]);
-        nombreBotonesCategorias.push(jsonResponseA[i]["name"])
+        addCategoria(i,jsonResponseA[i]); //inclusion de todas las categorias
     }
 }).catch((respuesta) => {
     console.log(respuesta)
@@ -36,10 +33,10 @@ pA.then((respuestaA) => {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //------------------------------------------Funciones de actualización------------------------------------------//
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+let listaProductosCarrito=[];
+let nItemsCarrito=0;
 /** Funcion para cambiar la categoria
  * Agrega la categoria como un div col con un boton interno
- * FALTA ASOCIAR FUNCION DE DISPLAY DE CARDS
  *  */
 function cambiarCategoria(i,data){
     categoriaActual=data["name"];
@@ -50,6 +47,43 @@ function cambiarCategoria(i,data){
         displayCards(jsonResponseA[i]["products"]);
     })
     
+}
+/** Funcion para agregar un producto al carrito
+ * Agrega una lista con los datos del producto a la lista del carrito
+ *  */
+function agregarProductoAcarrito(dataProduct,title){
+    let yaIngresado=false;
+    let theProduct=[];
+    for (k=0;k<dataProduct.length;k++){
+        if (dataProduct[k]["name"]==title){
+            theProduct=dataProduct[k];
+        }
+    }
+    if(listaProductosCarrito.length==0){
+        listaProductosCarrito=[[1,1,theProduct["name"],theProduct["price"],parseFloat(theProduct["price"])]];
+        nItemsCarrito++;
+    }
+    else{
+        for (i=0;i<listaProductosCarrito.length;i++){
+            if (listaProductosCarrito[i][2]==theProduct["name"]){
+                listaProductosCarrito[i]=[listaProductosCarrito[i][0],listaProductosCarrito[i][1]+1,listaProductosCarrito[i][2],listaProductosCarrito[i][3],listaProductosCarrito[i][4]+parseFloat(theProduct["price"])]
+                yaIngresado=true
+                nItemsCarrito++;
+            }
+        }
+        if (!yaIngresado){
+            listaProductosCarrito.push([listaProductosCarrito.length,1,theProduct["name"],theProduct["price"],parseFloat(theProduct["price"])])
+            nItemsCarrito++;
+        }
+    }
+    let nIcons=document.querySelector(".topIconsItems");
+    let h6=document.createElement("h6");
+    while (nIcons.firstChild) {
+        nIcons.removeChild(nIcons.lastChild);
+      } 
+    h6.innerHTML=nItemsCarrito+" items";
+    nIcons.appendChild(h6);
+    console.log(listaProductosCarrito);  
 }
 /** Funcion para agregar cada una de las categorias en el menu de busqueda
  * Agrega la categoria como un div col con un boton interno
@@ -67,7 +101,7 @@ function addCategoria(i,data) {
     col.appendChild(categoriaButton);
     div.appendChild(col);
 }
-/** Funcion para agregar actualizar la categoria actual
+/** Funcion para agregar actualizar el título de la categoria actual
  *  */
 function displayCategoriaActual(categoria){
     let div = document.querySelector(".TituloCategoria");
@@ -77,6 +111,18 @@ function displayCategoriaActual(categoria){
     let h2 = document.createElement("h2");
     h2.innerHTML=categoria;
     div.appendChild(h2);
+}
+/** Funcion para ocultar menu y mostrar carrito actual
+ *  */
+function showTable(){
+    let order=document.querySelector(".orderSection");
+    if(order.className=="orderSection collapse"){
+        order.className="orderSection collapse.show";
+    }else{
+        order.className="orderSection collapse";
+    } 
+    let menu=document.querySelector(".menuSection");
+    menu.className="orderSection collapse";
 }
 /** Funcion para agregar cards segun lista recibida
  *  */
@@ -105,7 +151,6 @@ function displayCards(products){
     cardText.className="card-text";
     cardButton.className="btn btn-dark ";
     cardButton.id=products[j]["name"];
-    cardButton.onclick()=function(){};
     
     currentImg.src=products[j]["image"];
     currentImg.alt="Card image cap";
@@ -113,6 +158,7 @@ function displayCards(products){
     cardText.innerHTML=products[j]["description"];
     cardPrice.innerHTML=products[j]["price"];
     cardButton.innerHTML="Add to car";
+    cardButton.onclick=function(){agregarProductoAcarrito(products,cardTitle.innerHTML);};
 
     cardBody.appendChild(cardTitle);
     cardBody.appendChild(newLine);
